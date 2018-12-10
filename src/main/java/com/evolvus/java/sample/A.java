07d905dfe0b37cba3e6e5e4ef1c6b4843a0ab4de
Kumaresan BK <EVOLVUS\kumaresanb@ELPBLR-035.evolvus.com>
2018-12-10 07:50:24 +0530

    deleed class

--- src/main/java/com/evolvus/java/sample/A.java
+++
@@ -1,9 +0,0 @@
-package com.evolvus.java.sample;
-
-public class A {
-	
-	public void test() {
-		System.out.println("A class test");
-	}
-
-}
--- src/main/java/com/evolvus/java/sample/B.java
+++
@@ -1,9 +0,0 @@
-package com.evolvus.java.sample;
-
-public class B extends A {
-	@Override
-	public void test() {
-		System.out.println("A override class in B");
-		super.test();
-	}
-}
